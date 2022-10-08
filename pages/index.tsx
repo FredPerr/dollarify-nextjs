b4e9f1e377a109bb2b48f0542cc1b9531842c82f
Frederic Perron <frederic.norrep@gmail.com>
2022-10-07 20:51:39 -0400

    npm added tRPC and remove home page content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,12 @@
 import Layout from 'layouts'
 import { ReactElement } from 'react'
 import { NextPageWithLayout } from 'layouts'
-import { Button } from '@/components/common/button/'
-import { AnchorText } from '@/components/common/anchor'
-import { AnchorButton } from '@/components/common/anchor'
-import { Box } from '@/components/common/box'
-import { Typography } from '@mui/material'
-import { Text } from '@/components/common/text'
 
 
 const Home: NextPageWithLayout = () => {
   return (
     <>
-      <Button variant="contained" color="error">test</Button>
-      <Button color="info">test</Button>
-      <Button color="primary">test</Button>
-      <Button color="secondary">test</Button>
-      <AnchorText href="/test" content="Do ut et commodo eiusmod."/>
-      <AnchorButton variant="contained" href="/test" content="Do ut et commodo eiusmod."/>
-      <Box variant='filled'><Typography>TEST</Typography></Box>
-      <Box variant='outlined'><Typography>TEST</Typography></Box>
-      <Text variant="h4">Test</Text>
-      <Text variant="overline">Test</Text>
+      
     </>
   )
 }
