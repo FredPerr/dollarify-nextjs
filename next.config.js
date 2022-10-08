d30e835d86a21fedccd562cc02084a157c3ae575
Frederic Perron <frederic.norrep@gmail.com>
2022-10-08 11:19:50 -0400

    added SVGR

--- next.config.js
+++ next.config.js
@@ -2,6 +2,23 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      resourceQuery: { not: [/url/] }, // exclude react component if *.svg?url
+      use: ["@svgr/webpack"]
+    },
+      {
+        test: /\.svg$/i,
+        type: 'asset',
+        resourceQuery: /url/,
+
+      })
+
+    return config
+  },
 }
 
 module.exports = nextConfig
