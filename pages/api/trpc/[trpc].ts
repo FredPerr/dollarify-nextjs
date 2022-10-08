a6877f9c93c552fca16c63fd491bc1f59308e158
Frederic Perron <frederic.norrep@gmail.com>
2022-10-08 10:19:25 -0400

    fixed full refresh bug with tRPC

--- pages/api/trpc/[trpc].ts
+++ pages/api/trpc/[trpc].ts
@@ -14,14 +14,13 @@ export const appRouter = trpc
       .nullish(),
     resolve({ input }) {
       return {
-        greeting: `hello ${input?.text ?? 'world'}`,
+        greeting: `Hi ${input?.text ?? 'world'}`,
       };
     },
   });
-// export type definition of API
-export type AppRouter = typeof appRouter;
-// export API handler
+
+  // export API handler
 export default trpcNext.createNextApiHandler({
-  router: appRouter,
+  router: serverRouter,
   createContext: createContext,
 });
